e76c527ec1667380ff2533717200928a81902b90
Jakub Kliś <jakubklis2201@gmail.com>
2024-04-16 11:32:40 +0200

    Change default font to Space Grotesk

--- SneakerFinder/Client/tailwind.config.ts
+++ SneakerFinder/Client/tailwind.config.ts
@@ -4,13 +4,16 @@ const config: Config = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Space Grotesk', 'sans-serif'],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
