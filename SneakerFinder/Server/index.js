db7f41f9bf92f991919cafbdaf7d23effb3bbf64
jklis2 <jakubklis2201@gmail.com>
2024-04-20 12:28:44 +0200

    Change saving place and format

--- SneakerFinder/Server/index.js
+++ SneakerFinder/Server/index.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 import { scrape } from "./scrape.js";
 
 const URL = "https://stockx.com/adidas-yeezy-slide-black-onyx";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 function saveData(data, filePrefix) {
   const timestamp = +new Date();
+  const dirPath = join(__dirname, 'data');
 
-  fs.writeFileSync(`${filePrefix}-${timestamp}.csv`, data);
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+
+  const filePath = join(dirPath, `${filePrefix}-${timestamp}.json`);
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
 scrape(URL, saveData);
