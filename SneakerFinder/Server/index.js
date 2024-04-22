a85ca47b8e3e6045c22e496b2211feadda216d96
RobertSternal <robst@onet.pl>
2024-04-22 18:29:05 +0200

    Change Scrape and Index.js

--- SneakerFinder/Server/index.js
+++ SneakerFinder/Server/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import puppeteer from "puppeteer";
 import Stockx from "./models/stockx.model.js";
-
 import connectMongoDB from "./mongodb.js";
+import { scrape } from "./scrape.js";
 
+dotenv.config();
 connectMongoDB();
 
 const URL = "https://stockx.com/adidas-yeezy-slide-black-onyx";
@@ -18,26 +18,6 @@ function saveData(data, filePrefix) {
     .save()
     .then(() => console.log("Data saved to MongoDB!"))
     .catch((err) => console.error("Error saving data to MongoDB:", err));
-}
-
-async function scrape(url, writeDataFn) {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
-  await page.goto(url);
-
-  await page.setViewport({ width: 1080, height: 1024 });
-
-  await page.waitForFunction(
-    () => !!document.querySelector('[data-component="Header"]')
-  );
-
-  const title = await page.$eval('[data-component="Header"]', (el) =>
-    el.textContent.trim()
-  );
-
-  await browser.close();
-  writeDataFn(title, "payload");
 }
 
 scrape(URL, saveData);
--- SneakerFinder/Server/models/stockx.model.js
+++ SneakerFinder/Server/models/stockx.model.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const stocksSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
   },
   price: {
     type: Number,
-    required: true,
   },
 });
 
